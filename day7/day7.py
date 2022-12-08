a06605910f5de95d38f5dbeafdf93f7743ba7424
Freddie <freddiepalfreman@hotmail.com>
2022-12-08 15:28:06 +0000

    Day 7 implemented as dicts and prints file system

--- day7/day7.py
+++ day7/day7.py
@@ -2,15 +2,6 @@ from pprint import pprint
 
 folders = []
 
-def navigate_to_active_directory(filesystem, active_dir):
-    """
-    Navigates to the active directory.
-    Returns the active directory.
-    """
-    temp_dir = filesystem
-    for index in active_dir:
-        temp_dir = temp_dir[index]
-    return temp_dir
 
 def search_folder(search, folder_name):
     """
@@ -18,58 +9,78 @@ def search_folder(search, folder_name):
     If the folder is found, returns the index of the folder.
     If the folder is not found, returns -1.
     """
-    for file in search:
-        if type(file) == list:
-            if file[0] == folder_name:
-                return search.index(file)
+    for file in search['children']:
+        if file['type'] == "folder":
+            if file['name'] == folder_name:
+                return search['children'].index(file)
     return -1
+
+
+def navigate_to_active_directory(filesystem, active_dir):
+    """
+    Navigates to the active directory.
+    Returns the active directory.
+    """
+    temp_dir = filesystem
+    for folder in active_dir:
+        temp_dir = temp_dir["children"][search_folder(temp_dir, folder)]
+    return temp_dir
+
 
 def create_filesystem():
     with open("day7/input.txt", "r") as f:
-        filesystem = [["/"]]
-        active_dir = [0]
+        filesystem = {"type": "folder", "name": "/", "children": []}
+        active_dir = []
         data = f.read().splitlines()
 
         for line in data:
-            line=line.split()
+            line = line.split()
             # user-typed command
             if line[0] == "$":
                 if line[1] == "cd":
                     if line[2] == "..":
                         active_dir.pop()
                     elif line[2] == "/":
-                        active_dir = [0]
+                        active_dir = []
                     else:
-                        temp_dir = navigate_to_active_directory(filesystem, active_dir)
+                        temp_dir = navigate_to_active_directory(
+                            filesystem, active_dir)
                         # look for the folder.
                         # if no folder exists with the given name, create it.
                         index = search_folder(temp_dir, line[2])
                         if index > -1:
-                            active_dir.append(index)
+                            active_dir.append(line[2])
                         else:
-                            temp_dir.append([line[2]])
-                            active_dir.append(temp_dir.index([line[2]]))
+                            new_folder = {"type": "folder",
+                                          "name": [line[2]], "children": []}
+                            temp_dir['children'].append(new_folder)
+                            active_dir.append(
+                                temp_dir['children'].index(new_folder))
             # ls output
             else:
                 temp_dir = navigate_to_active_directory(filesystem, active_dir)
                 if line[0] == "dir":
                     if search_folder(temp_dir, line[1]) == -1:
-                        temp_dir.append([line[1]])
+                        temp_dir['children'].append(
+                            {"type": "folder", "name": line[1], "children": []})
                 # if not a folder, it is a file with a size
                 else:
-                    temp_dir.append({"name": line[1],
-                                    "size": line[0]})
+                    temp_dir['children'].append(
+                        {"type": "file", "name": line[1], "size": line[0]})
         return filesystem
 
-def find_folder_sizes(filesystem):
+
+def find_folder_sizes(filesystem, level=0):
     total_size = 0
-    for file in filesystem:
-        if type(file) == dict:
+    for file in filesystem['children']:
+        if file['type'] == 'file':
             total_size += int(file["size"])
-        elif type(file) == list:
-            total_size += find_folder_sizes(file)
-    folders.append({"name": filesystem[0], "size": total_size})
+        elif file['type'] == 'folder':
+            print(f"{'  '*level+'/'}{file['name']}")
+            total_size += find_folder_sizes(file, level+1)
+    folders.append({"name": filesystem['name'], "size": total_size})
     return total_size
+
 
 def part_one():
     total = 0
@@ -77,6 +88,7 @@ def part_one():
         if folder["size"] < 100000:
             total += folder["size"]
     print("Part 1:", total)
+
 
 def part_two(total_size_used):
     space_to_free = -(70000000 - total_size_used - 30000000)
@@ -87,9 +99,10 @@ def part_two(total_size_used):
                 best_candidate = folder
     print("Part 2:", best_candidate)
 
+
 if __name__ == "__main__":
     filesystem = create_filesystem()
-    size_used = find_folder_sizes(filesystem[0])
+    size_used = find_folder_sizes(filesystem)
 
     part_one()
     part_two(size_used)
