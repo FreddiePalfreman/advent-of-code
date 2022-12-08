47e09c2d6c5de8559277b19344b51fc536fc217d
Freddie <freddiepalfreman@hotmail.com>
2022-12-08 14:22:19 +0000

    Day 7 split into functions

--- day7/day7.py
+++ day7/day7.py
@@ -1,7 +1,28 @@
 from pprint import pprint
-import sys
 
 folders = []
+
+def navigate_to_active_directory(filesystem, active_dir):
+    """
+    Navigates to the active directory.
+    Returns the active directory.
+    """
+    temp_dir = filesystem
+    for index in active_dir:
+        temp_dir = temp_dir[index]
+    return temp_dir
+
+def search_folder(search, folder_name):
+    """
+    Searches a folder for a folder with the given name.
+    If the folder is found, returns the index of the folder.
+    If the folder is not found, returns -1.
+    """
+    for file in search:
+        if type(file) == list:
+            if file[0] == folder_name:
+                return search.index(file)
+    return -1
 
 def create_filesystem():
     with open("day7/input.txt", "r") as f:
@@ -11,6 +32,7 @@ def create_filesystem():
 
         for line in data:
             line=line.split()
+            # user-typed command
             if line[0] == "$":
                 if line[1] == "cd":
                     if line[2] == "..":
@@ -18,51 +40,38 @@ def create_filesystem():
                     elif line[2] == "/":
                         active_dir = [0]
                     else:
-                        #go to given directory
-                        temp_dir = filesystem
-                        for index in active_dir:
-                            temp_dir = temp_dir[index]
-                        found = False
-                        for file in temp_dir:
-                            if type(file) == list:
-                                if file[0] == line[2]:
-                                    found = True
-                                    active_dir.append(temp_dir.index(file))
-                        if not found:
+                        temp_dir = navigate_to_active_directory(filesystem, active_dir)
+                        # look for the folder.
+                        # if no folder exists with the given name, create it.
+                        index = search_folder(temp_dir, line[2])
+                        if index > -1:
+                            active_dir.append(index)
+                        else:
                             temp_dir.append([line[2]])
                             active_dir.append(temp_dir.index([line[2]]))
-                elif line[1] == "ls":
-                    pass
-
-            # output from ls command
+            # ls output
             else:
-                temp_dir = filesystem
-                for index in active_dir:
-                    temp_dir = temp_dir[index]
+                temp_dir = navigate_to_active_directory(filesystem, active_dir)
                 if line[0] == "dir":
-                    found = False
-                    for file in temp_dir:
-                        if type(file) == list:
-                            if file[0] == line[1]:
-                                found = True
-                    if not found:
+                    if search_folder(temp_dir, line[1]) == -1:
                         temp_dir.append([line[1]])
+                # if not a folder, it is a file with a size
                 else:
                     temp_dir.append({"name": line[1],
                                     "size": line[0]})
         return filesystem
 
-def recursive_file_sizes(filesystem):
+def find_folder_sizes(filesystem):
     total_size = 0
     for file in filesystem:
         if type(file) == dict:
             total_size += int(file["size"])
         elif type(file) == list:
-            total_size += recursive_file_sizes(file)
+            total_size += find_folder_sizes(file)
     folders.append({"name": filesystem[0], "size": total_size})
     return total_size
 
-def part_one(filesystem):
+def part_one():
     total = 0
     for folder in folders:
         if folder["size"] < 100000:
@@ -78,8 +87,9 @@ def part_two(total_size_used):
                 best_candidate = folder
     print("Part 2:", best_candidate)
 
-filesystem = create_filesystem()
-size_used = recursive_file_sizes(filesystem[0])
+if __name__ == "__main__":
+    filesystem = create_filesystem()
+    size_used = find_folder_sizes(filesystem[0])
 
-part_one(filesystem)
-part_two(size_used)
+    part_one()
+    part_two(size_used)
